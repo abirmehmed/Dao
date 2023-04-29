40ee45c3093e9e12e2de8c0ae622eb71d2213614
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-04-29 20:37:55 +0600

    Update Dao.sol

--- Dao.sol
+++ Dao.sol
@@ -1,25 +1,44 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.0;
+
 contract DAO {
     struct Proposal {
         uint id;
         string description;
         uint voteCount;
+        mapping(address => bool) voted;
     }
+
     mapping(address => bool) public members;
     mapping(uint => Proposal) public proposals;
     uint public numProposals;
-    constructor() public {
+
+    constructor() {
         members[msg.sender] = true;
     }
+
+    function addMember(address _member) public {
+        require(members[msg.sender], "Only members can add new members.");
+        members[_member] = true;
+    }
+
+    function removeMember(address _member) public {
+        require(members[msg.sender], "Only members can remove members.");
+        require(_member != msg.sender, "Cannot remove self.");
+        members[_member] = false;
+    }
+
     function addProposal(string memory _description) public {
         require(members[msg.sender], "Only members can add proposals.");
         numProposals++;
         proposals[numProposals] = Proposal(numProposals, _description, 0);
     }
+
     function vote(uint _proposalId) public {
         require(members[msg.sender], "Only members can vote.");
+        require(!proposals[_proposalId].voted[msg.sender], "Member has already voted on this proposal.");
         proposals[_proposalId].voteCount++;
+        proposals[_proposalId].voted[msg.sender] = true;
     }
 }
