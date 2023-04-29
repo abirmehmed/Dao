257cc52e9d405eca77af5a2652b2557c9884f174
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-04-29 20:44:41 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 
 const web3 = new Web3(Web3.givenProvider);
 const daoAddress = '<DAO_CONTRACT_ADDRESS>';
 const daoABI = <DAO_CONTRACT_ABI>;
 const DAO = new web3.eth.Contract(daoABI, daoAddress);
+
 function App() {
     const [proposals, setProposals] = useState([]);
+    const [error, setError] = useState(null);
+
     async function getProposals() {
-        const numProposals = await DAO.methods.numProposals().call();
-        const proposals = [];
-        for (let i = 1; i <= numProposals; i++) {
-            const proposal = await DAO.methods.proposals(i).call();
-            proposals.push(proposal);
+        try {
+            const accounts = await web3.eth.getAccounts();
+            if (accounts.length === 0) {
+                setError('Please connect your Ethereum wallet.');
+                return;
+            }
+            const numProposals = await DAO.methods.numProposals().call();
+            const proposals = [];
+            for (let i = 1; i <= numProposals; i++) {
+                const proposal = await DAO.methods.proposals(i).call();
+                proposals.push(proposal);
+            }
+            setProposals(proposals);
+        } catch (err) {
+            setError(err.message);
         }
-        setProposals(proposals);
     }
+
+    useEffect(() => {
+        getProposals();
+    }, []);
+
     return (
         <div>
-            <button onClick={getProposals}>Get Proposals</button>
+            {error && <p>{error}</p>}
             <ul>
                 {proposals.map(proposal => (
                     <li key={proposal.id}>
@@ -29,4 +46,5 @@ function App() {
         </div>
     );
 }
+
 export default App;
